a74b99b60a14936b6d08b96d0df96b5427c20006
Mousemices <9.12716135xia@gmail.com>
2024-07-26 22:48:39 +0200

    codeforces: Bit++. The code has been improved according to the problem's condition.

--- Bit++/main.cpp
+++ Bit++/main.cpp
@@ -9,14 +9,10 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         cin >> operation;
-        if (operation.at(0) == 'X' || operation.at(operation.length() - 1) == 'X') {
-            if (operation.at(1) == '+') {
-                x++;
-            }
-
-            if (operation.at(1) == '-') {
-                x--;
-            }
+        if (operation.at(1) == '+') {
+            x++;
+        } else {
+            x--;
         }
     }
 
