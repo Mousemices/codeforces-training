c3949f7fe6f939942100f89b6ff895b6948093e3
Mousemices <9.12716135xia@gmail.com>
2024-07-28 23:44:13 +0200

    codeforces: Helpful Maths. Removed unecessary variables

--- Helpful_Maths/main.cpp
+++ Helpful_Maths/main.cpp
@@ -15,7 +15,7 @@ int main() {
         numbers.push_back((int) s.at(i) - '0'); // or s.at(i) - 48, both can typecast the numeric character into int accurately
     }
 
-    int temporary_number, temporary_index;
+    int temporary_number;
 
     // Brute-force sorting: Selection Sort with time complexity O(n^2)
     for (int i = 0; i < numbers.size(); ++i) {
@@ -29,12 +29,11 @@ int main() {
     }
 
     string ordered_s;
-    string temporary_s;
     string number;
 
     for (int i = 0; i < numbers.size(); ++i) {
         number = to_string(numbers[i]);
-        ordered_s = temporary_s.append(number).append(addition);
+        ordered_s.append(number).append(addition);
     }
 
     ordered_s.pop_back();
