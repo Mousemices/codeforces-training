ad36e9fded48fe5a40a88d6f68128dc4f79c9f11
Mousemices <9.12716135xia@gmail.com>
2024-08-01 13:53:00 +0200

    codeforces: Queue at the School, without using string a, make use if the string s

---
+++ Queue_At_The_School/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, t;
+    string s;
+
+    cin >> n >> t;
+    cin >> s;
+    
+    for (int i = 0; i < t; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (j + 1 < n && s.at(j) == 'B' && s.at(j+1) == 'G') {
+                s.at(j) = 'G';
+                s.at(j+1) = 'B';
+                j += 1;
+            }
+        }
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
