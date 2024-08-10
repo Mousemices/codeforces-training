773936a50683fb745b7b5563b02cbcab11ad1da9
Mousemices <9.12716135xia@gmail.com>
2024-08-10 20:00:24 +0200

    codeforces: Applied precise derived formula for Candies and Two Sisters problem

--- Candies_And_Two_Sisters/main.cpp
+++ Candies_And_Two_Sisters/main.cpp
@@ -8,28 +8,15 @@ int main() {
 
     cin >> t;
 
-    vector<long long> candies;
     vector<long long> ways_to_distribute_candies;
 
     for (long long i = 0; i < t; ++i) {
         cin >> n;
-        candies.push_back(n);
+        ways_to_distribute_candies.push_back((n-1) / 2);
     }
 
-    long long divided;
-
-    for (long long candy : candies) {
-        divided = candy / 2;
-
-        if (candy % 2 == 0) {
-            divided--;
-        }
-
-        ways_to_distribute_candies.push_back(divided);
-    }
-
-    for (int i = 0; i < ways_to_distribute_candies.size(); ++i) {
-        cout << ways_to_distribute_candies[i] << endl;
+    for (long long distributed : ways_to_distribute_candies) {
+        cout << distributed << endl;
     }
 
     return 0;
