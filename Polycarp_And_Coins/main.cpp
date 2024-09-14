cd290e4d3ceee9170a60d3aa24829edda1bdbded
Mousemices <9.12716135xia@gmail.com>
2024-09-14 18:36:11 +0200

    codeforces: Polycarp and Coins, minimize the c1 and c2 coins to be equally, make the difference as little as possible.

---
+++ Polycarp_And_Coins/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long long t, n, c1, c2;
+
+    cin >> t;
+
+    for (long long i = 0; i < t; ++i) {
+        cin >> n;
+
+        c1 = (long long) ceil((double) n / 3);
+        c2 = (long long) floor((double) n / 3);
+
+        cout << c1 << " " << c2 << endl;
+    }
+
+    return 0;
+}
