0837bdc1f3f245bb29d180ceb33a4f4d2eec1d60
Mousemices <9.12716135xia@gmail.com>
2024-09-15 17:24:02 +0200

    codeforces: Polycarp and Coins, make the c1 and c2 coins to be equally, make the difference of two types of coins as little as possible. Verdicted solution.

--- Polycarp_And_Coins/main.cpp
+++ Polycarp_And_Coins/main.cpp
@@ -11,14 +11,17 @@ int main() {
     for (long long i = 0; i < t; ++i) {
         cin >> n;
 
-        c1 = (long long) ceil((double) n / 3);
-        c2 = (long long) floor((double) n / 3);
+        c2 = n / 3;
+        c1 = n - 2 * c2;
 
-        if (c1 + c2 * 2 == n) {
-            cout << c1 << " " << c2 << endl;
-        } else {
-            cout << c2 << " " << c1 << endl;
+        if (n % 3 == 1) {
+            c1 = c2 + 1;
+        } else if (n % 3 == 2) {
+            c1 = c2;
+            c2 = c2 + 1;
         }
+
+        cout << c1 << " " << c2 << endl;
     }
 
     return 0;
